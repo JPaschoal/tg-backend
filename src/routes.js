712a592a2702c9ac82aa6d806960f46582acb75f
Jhonatan Paschoal <jhonatan.paschoal@gmail.com>
2021-03-06 16:38:59 -0300

    fatec api connection

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
+import apiFatec from './services/fatecAPI.js';
+
 const routes = Router();
 
 routes.get('/', (req, res) => {
-  res.send('Ta la menininho')
+  const myaccount = apiFatec("582728691SP", "30100505")
+  let loginState;
+  myaccount.login().then(loginState = "Logged").catch(loginState = "Fulldeu")
+  res.send(loginState)
 })
 
 routes.get('/test', (req, res) => {
