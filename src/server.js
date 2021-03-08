940097e90bbacb33e3a0c370acb7c45a2b31a628
MikaelOM <el.mh506@gmail.com>
2021-03-07 21:35:40 -0300

    conexao do banco de dados por um arquivo externo

--- src/server.js
+++ src/server.js
@@ -1,32 +1,17 @@
 import express from 'express';
-import bodyParser from "body-parser";
 import {Sequelize} from 'sequelize';
+import bodyParser from 'body-parser';
+import connection from './Database/configdb.js'
 
-//import connectDB from './Database/connection.js'
 import routes from './routes.js';
 
-
-const sequelize = new Sequelize ('esystem','root','123Mysql', {
-  host:'localhost',
-  dialect:'mysql'
-});
-
-try {
-  await sequelize.authenticate();
-   console.log('Successfull Connection');
-}catch(error){
-  console.log('connection error');
-}
-
 const app = express();
+const db = () =>{new Sequelize(connection)}
+
 app.use(express.json());
 app.use(routes)
+app.use(db);
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-//app.use(connectDB);
-
-
 
 
 app.listen(3333);
