52cd9f845a5ec279443ca8abff5e060c5ea688cc
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2021-03-27 00:19:24 -0300

    refactoring controllers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import apiFatec from '../services/fatecAPI.js';
+import isLogged from '../utils/isLogged.js';
 
 let myaccount;
 
@@ -6,97 +7,85 @@ export default {
   async login(request, response) {
     const {login, password} = request.body
     myaccount = apiFatec(login, password);
-    let status;
     await myaccount.login().then(
       () => {
-        status = myaccount.isLogged() ? "logged" : "notLogged";
+        const status = myaccount.isLogged() ? "logged" : "notLogged";
+        console.log(status)
+        return response.json({"status": status});
       })
       .catch((err) => {
-        console.log(err);
-        status = myaccount.isDenied() ? "notLogged" : "logged";
-      }) 
-      console.log(status)
-    return response.json({"status": status});
-  },
+        console.error(err);
+      })
+    },
 
   async perfil(request, response) {
-    let profile;
     if(!myaccount) {
       return response.json({status: "notLogged"});
     }
     await myaccount.getProfile()
-    .then( result => {
-      profile = result;
+    .then( profile => {
+      console.log(profile);
+      return response.json(profile);
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
     })
-    console.log(profile);
-    return response.json(profile);
   },
 
   async schedule(request, response) {
-    let schedule;
     if(!myaccount) {
       return response.json({status: "notLogged"});
     }
     await myaccount.getSchedules()
-    .then( result => {
-      schedule = result;
+    .then( schedule => {
+      console.log(schedule);
+      return response.json(schedule);
     })
     .catch((err) =>{
-      console.log(err);
+      console.error(err);
     })
-    console.log(schedule);
-    return response.json(schedule);
   },
 
   async enrolledDisciplines(request, response) {
-    let enrolledDisciplines;
     if(!myaccount) {
       return response.json({status: "notLogged"});
     }
     await myaccount.getEnrolledDisciplines()
-    .then( result => {
-      enrolledDisciplines = result;
+    .then( enrolledDisciplines => {
+      console.log(enrolledDisciplines);
+      return response.json(enrolledDisciplines);
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
     });
-    console.log(enrolledDisciplines);
-    return response.json(enrolledDisciplines);
   },
 
   async calendar(request, response) {
-    let calendar;
     if(!myaccount) {
       return response.json({status: "notLogged"});
     }
     await myaccount.getAcademicCalendar()
-    .then( result => {
-      calendar = result;
+    .then( calendar => {
+      console.log(calendar);
+      return response.json(calendar);
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
     });
-    console.log(calendar);
-    return response.json(calendar);
   },
 
   async grades(request, response) {
-    let grades;
     if(!myaccount) {
       return response.json({status: "notLogged"});
     }
     await myaccount.getPartialGrades()
-    .then( result => {
-      grades = result;
+    .then( grades => {
+      console.log(grades);
+      return response.json(grades);
     })
     .catch((err) => {
-      console.log(err);
+      console.error(err);
     });
-    console.log(grades);
-    return response.json(grades);
   },
 
   logout(request, response) {
