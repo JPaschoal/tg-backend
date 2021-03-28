62a26bc2da307640f3c5708bf59f20a9a34ab5ef
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2021-03-28 12:34:34 -0300

    adding notebooks and notes routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,18 +3,40 @@ import userController from './controllers/userController'
 
 const routes = Router();
 
-routes.post('/login', userController.login);
+routes.post('/login', userController.login)
 
-routes.get('/perfil', userController.perfil);
+routes.get('/perfil', userController.perfil)
 
-routes.get('/schedule', userController.schedule);
+routes.get('/schedule', userController.schedule)
 
-routes.get('/enrolledDisciplines', userController.enrolledDisciplines);
+routes.get('/enrolledDisciplines', userController.enrolledDisciplines)
 
-routes.get('/calendar', userController.calendar);
+routes.get('/calendar', userController.calendar)
 
-routes.get('/grades', userController.grades);
+routes.get('/grades', userController.grades)
 
 routes.get('/logout', userController.logout)
+
+// routes.get('/notebooks/list', notebooksController.list)
+
+// routes.post('/notebooks/create', notebooksController.create)
+
+// routes.delete('/notebooks/delete', notebooksController.delete)
+
+// routes.post('/notebooks/update', notebooksController.update)
+
+// routes.get('/notes/list', notesController.list)
+
+// routes.post('/notes/create', notesController.create)
+
+// routes.delete('/notes/delete', notesController.delete)
+
+// routes.post('/notes/update', notesController.update)
+
+// routes.get('/notes/files/list' notesController.listFiles)
+
+// routes.post('/notes/files/save' notesController.saveFiles)
+
+// routes.delete('/notes/files/delete' notesController.deleteFiles)
 
 export default routes;
