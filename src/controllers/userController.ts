83d1c59d0019f825b3ffcb4211425ba86cafc643
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2021-03-28 00:53:46 -0300

    Verefy ra

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,9 @@
 import apiFatec from '../services/fatecAPI';
-import { IMyAccount, IProfile, ISchedule, IEnrolledDisciplines, ICalendar, IGrades,  IAccess } from '../typings/fatecApi'
 import {  Request, Response } from 'express'
+import { getRepository } from 'typeorm';
+
+import { IMyAccount, IProfile, ISchedule, IEnrolledDisciplines, ICalendar, IGrades,  IAccess } from '../typings/fatecApi'
+import Student from '../Models/Student'
 
 let myaccount: IMyAccount | null;
 
@@ -12,12 +15,34 @@ export default {
     try {
       await myaccount.login()
       const status: boolean = myaccount.isLogged()
+
+      if(status){
+        try {
+          const profile: IProfile = await myaccount.getProfile()
+          const ra = profile.code
+          const studentRepository = getRepository(Student)
+
+          const searchRA = await studentRepository.find({ where: { ra }})
+          if(searchRA.length === 0){
+            console.log('No result')
+            const student = studentRepository.create({ ra, profile_image: 'empty' })
+            await studentRepository.save(student)
+          }
+          else {
+            console.log('Already exists')
+          }
+        } 
+        catch (err) {
+          console.error(err)
+        }
+      }
+
       return response.json({ "status": status })
     }
     catch (err) {
       console.error("invalido");
       console.error(err)
-      return response.json({ "status": "notLogged" })
+      return response.status(200).json({ "status": "notLogged" })
     }
     },
 
